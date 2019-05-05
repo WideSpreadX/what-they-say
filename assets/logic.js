db03a7ee26f17dbc0977e865f5939d0a77bdd77c
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2019-05-05 15:56:47 -0400

    Initial Upload for Jonny O local drive

---
+++ assets/logic.js
@@ -0,0 +1,28 @@
+function overlayShow() {
+    document.getElementById("overlay").className += " show";
+}
+
+function overlayHide() {
+    document.getElementById("overlay").classList.remove("show");
+}
+
+
+/*-----------------Smiley Selection Function----------------*/
+
+var faces = {
+    happy: happyFace,
+    thinking: thinkingFace,
+    sad: sadFace
+}
+
+function() {
+    receiveScore
+}
+var getFaces = showFace; 
+
+
+
+document.getElementById("resultsTable")
+document.getElementById("sadFace");
+document.getElementById("thinkingFace");
+document.getElementById("happyFace");
